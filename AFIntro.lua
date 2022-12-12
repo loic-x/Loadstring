05bf1518337f159bfede0f8bcfb066e0a19b943a
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2022-12-12 17:39:22 -0500

    Create AFIntro.lua

---
+++ AFIntro.lua
@@ -0,0 +1,222 @@
+print("HarshTech Version 7.5 Has Been Loaded!")
+print("Also Hi i love Hentai and you are a pervert lol")
+AF = {} 
+setmetatable(AF, {__metatable = "My Table Pervert"})
+
+AF.ScreenGui1 = Instance.new("ScreenGui", game:GetService("CoreGui"))
+AF.Frame1 = Instance.new("Frame", AF.ScreenGui1)
+AF.ImageLabel1 = Instance.new("ImageLabel", AF.Frame1)
+AF.Frame2 = Instance.new("Frame", AF.Frame1)
+AF.TextLabel1 = Instance.new("TextLabel", AF.Frame2)
+AF.Frame3 = Instance.new("Frame", AF.Frame1)
+AF.TextLabel2 = Instance.new("TextLabel", AF.Frame3)
+
+AF.ScreenGui1.DisplayOrder = 0
+AF.ScreenGui1.Enabled = true
+AF.ScreenGui1.IgnoreGuiInset = false
+AF.ScreenGui1.Name = "HarshTechV7.5 Has Now Been Loaded"
+AF.ScreenGui1.ResetOnSpawn = true
+AF.ScreenGui1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+AF.ScreenGui1.Archivable = true
+AF.ScreenGui1.AutoLocalize = true
+
+AF.Frame1.Active = false
+AF.Frame1.AnchorPoint = Vector2.new(0.5, 0.5)
+AF.Frame1.BackgroundColor3 = Color3.new(1, 1, 1)
+AF.Frame1.BackgroundTransparency = 1
+AF.Frame1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+AF.Frame1.BorderSizePixel = 1
+AF.Frame1.LayoutOrder = 0
+AF.Frame1.Name = "SupportFrame"
+AF.Frame1.Position = UDim2.new(0.5, 0, 0.5, 0)
+AF.Frame1.Rotation = 0
+AF.Frame1.Selectable = false
+AF.Frame1.Size = UDim2.new(0, 546, 0, 100)
+AF.Frame1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+AF.Frame1.Style = Enum.FrameStyle.Custom
+AF.Frame1.Visible = true
+AF.Frame1.ZIndex = 1
+AF.Frame1.Archivable = true
+AF.Frame1.ClipsDescendants = false
+AF.Frame1.AutoLocalize = true
+
+AF.ImageLabel1.Active = false
+AF.ImageLabel1.AnchorPoint = Vector2.new(0, 0)
+AF.ImageLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+AF.ImageLabel1.BackgroundTransparency = 1
+AF.ImageLabel1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+AF.ImageLabel1.BorderSizePixel = 0
+AF.ImageLabel1.LayoutOrder = 0
+AF.ImageLabel1.Name = "AFIntro"
+AF.ImageLabel1.Position = UDim2.new(0.41844692826271057, 0, 0.04678363725543022, 0)
+AF.ImageLabel1.Rotation = 0
+AF.ImageLabel1.Selectable = false
+AF.ImageLabel1.Size = UDim2.new(0, 90, 0, 90)
+AF.ImageLabel1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+AF.ImageLabel1.Visible = true
+AF.ImageLabel1.ZIndex = 1
+AF.ImageLabel1.Archivable = true
+AF.ImageLabel1.ClipsDescendants = false
+AF.ImageLabel1.AutoLocalize = true
+AF.ImageLabel1.Image = "http://www.roblox.com/assest/?id=9863339777"
+AF.ImageLabel1.ImageColor3 = Color3.new(1, 1, 1)
+AF.ImageLabel1.ImageRectOffset = Vector2.new(0, 0)
+AF.ImageLabel1.ImageRectSize = Vector2.new(0, 0)
+AF.ImageLabel1.ImageTransparency = 1
+AF.ImageLabel1.ScaleType = Enum.ScaleType.Stretch
+AF.ImageLabel1.SliceScale = 1
+
+AF.Frame2.Active = false
+AF.Frame2.AnchorPoint = Vector2.new(0, 0)
+AF.Frame2.BackgroundColor3 = Color3.new(1, 1, 1)
+AF.Frame2.BackgroundTransparency = 1
+AF.Frame2.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+AF.Frame2.BorderSizePixel = 1
+AF.Frame2.LayoutOrder = 0
+AF.Frame2.Name = "RightBlock"
+AF.Frame2.Position = UDim2.new(0, 0, 0, 0)
+AF.Frame2.Rotation = 0
+AF.Frame2.Selectable = false
+AF.Frame2.Size = UDim2.new(0, 227, 0, 100)
+AF.Frame2.SizeConstraint = Enum.SizeConstraint.RelativeXY
+AF.Frame2.Style = Enum.FrameStyle.Custom
+AF.Frame2.Visible = true
+AF.Frame2.ZIndex = 1
+AF.Frame2.Archivable = true
+AF.Frame2.ClipsDescendants = true
+AF.Frame2.AutoLocalize = true
+
+
+AF.TextLabel1.Active = false
+AF.TextLabel1.AnchorPoint = Vector2.new(0, 0)
+AF.TextLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+AF.TextLabel1.BackgroundTransparency = 1
+AF.TextLabel1.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+AF.TextLabel1.BorderSizePixel = 1
+AF.TextLabel1.LayoutOrder = 0
+AF.TextLabel1.Name = "AF1"
+AF.TextLabel1.Position = UDim2.new(0.996688961982727, 0, 0.25017544627189636, 0)
+AF.TextLabel1.Rotation = 0
+AF.TextLabel1.Selectable = false
+AF.TextLabel1.Size = UDim2.new(0, 228, 0, 50)
+AF.TextLabel1.SizeConstraint = Enum.SizeConstraint.RelativeXY
+AF.TextLabel1.Visible = true
+AF.TextLabel1.ZIndex = 1
+AF.TextLabel1.Archivable = true
+AF.TextLabel1.ClipsDescendants = false
+AF.TextLabel1.AutoLocalize = true
+AF.TextLabel1.Font = Enum.Font.Antique
+AF.TextLabel1.Text = "HarshTechV7.5"
+AF.TextLabel1.TextColor3 = Color3.new(1, 1, 1)
+AF.TextLabel1.TextScaled = false
+AF.TextLabel1.TextSize = 35
+AF.TextLabel1.TextStrokeColor3 = Color3.new(0, 0, 0)
+AF.TextLabel1.TextStrokeTransparency = 1
+AF.TextLabel1.TextTransparency = 0
+AF.TextLabel1.TextTruncate = Enum.TextTruncate.None
+AF.TextLabel1.TextWrapped = false
+AF.TextLabel1.TextXAlignment = Enum.TextXAlignment.Center
+AF.TextLabel1.TextYAlignment = Enum.TextYAlignment.Center
+
+
+AF.Frame3.Active = false
+AF.Frame3.AnchorPoint = Vector2.new(0, 0)
+AF.Frame3.BackgroundColor3 = Color3.new(1, 1, 1)
+AF.Frame3.BackgroundTransparency = 1
+AF.Frame3.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+AF.Frame3.BorderSizePixel = 1
+AF.Frame3.LayoutOrder = 0
+AF.Frame3.Name = "LeftBlock"
+AF.Frame3.Position = UDim2.new(0.5824176073074341, 0, 0, 0)
+AF.Frame3.Rotation = 0
+AF.Frame3.Selectable = false
+AF.Frame3.Size = UDim2.new(0, 228, 0, 100)
+AF.Frame3.SizeConstraint = Enum.SizeConstraint.RelativeXY
+AF.Frame3.Style = Enum.FrameStyle.Custom
+AF.Frame3.Visible = true
+AF.Frame3.ZIndex = 1
+AF.Frame3.Archivable = true
+AF.Frame3.ClipsDescendants = true
+AF.Frame3.AutoLocalize = true
+
+
+AF.TextLabel2.Active = false
+AF.TextLabel2.AnchorPoint = Vector2.new(0, 0)
+AF.TextLabel2.BackgroundColor3 = Color3.new(1, 1, 1)
+AF.TextLabel2.BackgroundTransparency = 1
+AF.TextLabel2.BorderColor3 = Color3.new(0.105882, 0.164706, 0.207843)
+AF.TextLabel2.BorderSizePixel = 1
+AF.TextLabel2.LayoutOrder = 0
+AF.TextLabel2.Name = "AF"
+AF.TextLabel2.Position = UDim2.new(-1.0022845268249512, 0, 0.25017544627189636, 0)
+AF.TextLabel2.Rotation = 0
+AF.TextLabel2.Selectable = false
+AF.TextLabel2.Size = UDim2.new(0, 228, 0, 50)
+AF.TextLabel2.SizeConstraint = Enum.SizeConstraint.RelativeXY
+AF.TextLabel2.Visible = true
+AF.TextLabel2.ZIndex = 1
+AF.TextLabel2.Archivable = true
+AF.TextLabel2.ClipsDescendants = false
+AF.TextLabel2.AutoLocalize = true
+AF.TextLabel2.Font = Enum.Font.Antique
+AF.TextLabel2.Text = "Has Been Loaded!"
+AF.TextLabel2.TextColor3 = Color3.new(1, 1, 1)
+AF.TextLabel2.TextScaled = false
+AF.TextLabel2.TextSize = 35
+AF.TextLabel2.TextStrokeColor3 = Color3.new(0, 0, 0)
+AF.TextLabel2.TextStrokeTransparency = 1
+AF.TextLabel2.TextTransparency = 0
+AF.TextLabel2.TextTruncate = Enum.TextTruncate.None
+AF.TextLabel2.TextWrapped = false
+AF.TextLabel2.TextXAlignment = Enum.TextXAlignment.Center
+AF.TextLabel2.TextYAlignment = Enum.TextYAlignment.Center
+
+-- Intro
+
+local getByName = function(N)
+    for i,v in pairs(AF) do
+        if v.Name and v.Name == N then
+            return v
+        end
+    end
+    return nil
+end
+
+local AF2 = getByName("AFIntro")
+local LeftSideText = getByName("AF1")
+local RightSideText = getByName("AF")
+
+while task.wait() do
+    if AF2.ImageTransparency == 0 then
+        break
+    end
+    AF2.ImageTransparency = AF2.ImageTransparency - 0.01
+end
+
+repeat task.wait() until AF2.ImageTransparency == 0
+
+local OriginalPositions = {}
+OriginalPositions[1] = LeftSideText.Position
+OriginalPositions[2] = RightSideText.Position
+
+LeftSideText:TweenPosition(UDim2.new(0, 0, 0.25, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+task.wait(1)
+RightSideText:TweenPosition(UDim2.new(0, 0, 0.25, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+
+task.wait(2)
+
+LeftSideText:TweenPosition(OriginalPositions[1], Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+task.wait(1)
+RightSideText:TweenPosition(OriginalPositions[2], Enum.EasingDirection.Out, Enum.EasingStyle.Quint, 1)
+task.wait(1)
+
+while task.wait() do
+    if AF2.ImageTransparency == 1 then
+        break
+    end
+    AF2.ImageTransparency = AF2.ImageTransparency + 0.01
+end
+
+repeat task.wait() until AF2.ImageTransparency == 1
+
+getByName("HarshTechV7.5 Has Now Been Loaded"):Destroy()
