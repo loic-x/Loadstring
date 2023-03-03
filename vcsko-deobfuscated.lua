1c01dc0c4fd114c4b39307a89a719912457e0156
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-03-03 13:48:34 -0500

    Create vcsko-deobfuscated.lua

---
+++ vcsko-deobfuscated.lua
@@ -0,0 +1,75 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+if game.PlaceId == 8146731988 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/UltraPowerTycoon/Script", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
+elseif game.PlaceId == 286090429 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/Arsenal/OPArsenal", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
+elseif game.PlaceId == 6872265039 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/Bedwars/OPBedwars", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
+elseif game.PlaceId == 4924922222 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/Brookhaven/p.lua", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
+elseif game.PlaceId == 621129760 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/K.A.T/K.lua", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
+elseif game.PlaceId == 11153589280 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/Doors-Race-Clicker/op.lua", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
+elseif game.PlaceId == 3101667897 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Games/Legends-Of-Speeds/l.lua", true))()
+Notification:Notify(
+    {Title = "Notification", Description = "Game Supported!"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+
+else
+
+Notification:Notify(
+    {Title = "Notification", Description = "Game Not Supported!, So here Vcsk V Hub V1.02"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 6.5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
+)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/VcskV/main/Hub/V1.0.2", true))()
+end
